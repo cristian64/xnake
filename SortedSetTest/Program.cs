69c012a138dec5bbc085d325346e75f3da24f688
cristian64@gmail.com <cristian64@gmail.com@fb917453-772a-b118-ac1c-f7af02810511>
2012-01-24 00:00:52 +0000

    A* added, including two projects to test it.

---
+++ SortedSetTest/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using AStar;
+
+namespace SortedSetTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            SortedSet<Node> nodes = new SortedSet<Node>(new NodeComparer());
+            for (int i = 0; i < 100; i++)
+            {
+                Node node = new Node(i, i + 100);
+                node.F = random.Next() % 100;
+                nodes.Add(node);
+                Console.Write(node.F + ", ");
+            }
+            Console.WriteLine();
+            while (nodes.Count > 0)
+            {
+                Console.Write(nodes.Max.F + ", ");
+                nodes.Remove(nodes.Max);
+            }
+            Console.WriteLine();
+
+            Console.ReadLine();
+        }
+    }
+}
